ec9a7fa7cf57990ab1ba0071b522b88066bdfdda
Adit Shinde <aditdineshshinde@gmail.com>
2018-12-20 23:21:34 +0530

    Inheritance

---
+++ oops/inheritance.py
@@ -0,0 +1,20 @@
+class Add( object ):
+    def add( self, num1, num2 ):
+        return num1 +  num2
+
+class Subtract( object ):
+    def subtract( self, num1, num2 ):
+        return num1 - num2
+
+class Multiply( object ):
+    def multiply( self, num1, num2 ):
+        return num1 * num2
+
+class Divide( object ):
+    def divide( self, num1, num2 ):
+        return num1 / num2
+
+class Arithmetic( Add, Subtract, Multiply, Divide ):
+    def __init__( self, num1=0, num2=0 ):
+        self.num1 = num1
+        self.num2 = num2
