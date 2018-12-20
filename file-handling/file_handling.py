c71cb16e03149f76b25dc3e213fd8e44259a7012
Adit Shinde <aditdineshshinde@gmail.com>
2018-12-20 22:31:31 +0530

    File Handling

---
+++ file-handling/file_handling.py
@@ -0,0 +1,28 @@
+#Creating new file
+outputFile = open(r'temp_file.txt','w')
+
+#write raw text
+outputFile.write('This line is from string')
+
+#write a list to file
+text = ['This','line','is','from','a','list']
+outputFile.writelines(text)
+
+#write a list to file
+text = ('This','line','is','from','a','tuple')
+outputFile.writelines(text)
+
+#close the file pointer
+outputFile.close()
+
+#read from the file
+inputFile = open(r'temp_file.txt','r')
+readText = inputFile.read()
+print(readText)
+inputFile.close()
+
+#read file into seperate lines
+inputFile = open(r'temp_file.txt','r')
+readText = inputFile.readlines()
+print(readText)
+inputFile.close()
