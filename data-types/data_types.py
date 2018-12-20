9676df2ce31ccde59ae803fd65b094a31d171bcf
Adit Shinde <aditdineshshinde@gmail.com>
2018-12-20 20:34:52 +0530

    Data types in python

---
+++ data-types/data_types.py
@@ -0,0 +1,33 @@
+#Single line comments start with '#'
+'''
+Multiline comments are enclosed within 6 single quotes.
+Lets start coding now.
+'''
+
+#Strings are enclosed in single or double quotes '' or ""
+a = 'xyz' #Sinngle quoted string
+print('Type of a: %s' %type(a))
+
+a = "xyz" #Double quoted string
+print('Type of a: %s' %type(a))
+
+#Integers are represented without decimal point.
+b = 1
+print('Type of b: %s' %type(b))
+
+#Floats are represented with decimal point.
+c = 1.0
+print('Type of b: %s' %type(c))
+
+#List is enclosed in square brackets.
+d = [1,2,3]
+print('Type of b: %s' %type(d))
+
+#Dictionary is enclosed in curly brackets.
+#Also called as a map
+e = { 'os': 'linux' }
+print('Type of b: %s' %type(e))
+
+#Tuple is enclosed in parenthesis.
+f = (1,2)
+print('Type of b: %s' %type(f))
