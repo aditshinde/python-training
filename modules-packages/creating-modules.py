4e6152f7a224c960622b748361ac27bec0fcd4d2
Adit Shinde <aditdineshshinde@gmail.com>
2018-12-20 21:46:45 +0530

    Creating new module

---
+++ modules-packages/creating-modules.py
@@ -0,0 +1,6 @@
+#Modules can be created with _ or camel case
+import first_module
+import secondModule
+
+#Importing python file under the module
+import third_module.foo
---
+++ modules-packages/importing-modules.py
@@ -0,0 +1,7 @@
+#Import the library/module
+import math
+print(math.sin(0))
+
+#Import only the functions/classes/objects
+from math import cos
+print(cos(0))
