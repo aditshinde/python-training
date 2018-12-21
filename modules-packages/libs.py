656758d17db75648525017110069ffb277637f86
Adit Shinde <aditdineshshinde@gmail.com>
2018-12-21 22:42:22 +0530

    Libraries

---
+++ modules-packages/libs.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import datetime
+
+print(datetime.date.today())
+print(datetime.datetime.today())
+print(datetime.datetime.now())
+
+#print(sys.modules)
+#print(sys.argv)
+#print(sys.maxint)
+#print(sys.platform)
+#print(sys.version)
+
+#print(os.listdir('./'))
+#os.mkdir('tempdir')
+#os.rmdir('tempdir')
+#print(os.environ.data)
+#print(os.getcwd())
+#print(os.system('gedit'))
