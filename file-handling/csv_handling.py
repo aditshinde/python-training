c8f8f4c9e42e9648d6d7ea27cfff694d45eaaf49
Adit Shinde <aditdineshshinde@gmail.com>
2018-12-21 14:04:39 +0530

    CSV Handling

---
+++ file-handling/csv_handling.py
@@ -0,0 +1,18 @@
+import csv
+
+data = [
+    ('ID','Title','Author'),
+    ('1','CRLS','Cormen'),
+    ('2','C for Dummies','Rayson'),
+    ]
+
+with open('AllBooks.csv','w') as outFile:
+    csv_writer = csv.writer(outFile)
+    for row in data:
+        csv_writer.writerow(row)
+
+with open('AllBooks.csv','r') as inFile:
+    csv_reader = csv.reader(inFile)
+    csvIterable = csv.reader(inFile)
+    for row in csvIterable:
+        print(row)
